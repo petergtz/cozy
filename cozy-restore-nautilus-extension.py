c6fd86a20d0090e471a3eb80d92ebba8b41473e6
peter <peter@pider.(none)>
2009-06-26 02:08:12 +0200

    icon setting correctly

--- cozy-restore-nautilus-extension.py
+++ cozy-restore-nautilus-extension.py
@@ -64,20 +64,20 @@ class CozyRestoreNautilusExtension(nautilus.MenuProvider):
 			self.session_bus = dbus.SessionBus()
 			self.manager = self.session_bus.get_object('org.freedesktop.Cozy', '/org/freedesktop/Cozy/Manager')
 			if not self.is_in_restore_mode:
-				item = nautilus.MenuItem(name="NautilusPython::restoremode", icon='document - open - recent', label="Go to Restore Mode", tip="Switches Nautilus into Bakcupmode to discover older versions of file and folders")
+				item = nautilus.MenuItem(name="NautilusPython::restoremode", icon='cozy', label="Start Restore Mode", tip="Switches Nautilus into Restore Mode to discover older versions of files and folders")
 				item.connect("activate", self.go_to_restore_mode, path, window)
 				items.append(item)
 			else:
-				item = nautilus.MenuItem(name="NautilusPython::closerestorebackupmode", label="Close Restore Mode", tip="", icon='exit')
+				item = nautilus.MenuItem(name="NautilusPython::closerestorebackupmode", label="Close Restore Mode", tip="Switches back to normal mode", icon='exit')
 				item.connect("activate", self.close_restore_mode, path, window)
 				items.append(item)
 
 				if self.manager.get_previous_version_path(path, dbus_interface='org.freedesktop.Cozy.Manager') != '':
-					item = nautilus.MenuItem(name="NautilusPython::prev", label="Previous Version", tip="Go to previous Version of current location", icon='go - previous')
+					item = nautilus.MenuItem(name="NautilusPython::prev", label="Previous Version", tip="Go to previous Version of current location", icon='go-previous')
 					item.connect("activate", self.go_to_previous_version, path, window)
 					items.append(item)
 				if self.manager.get_next_version_path(path, dbus_interface='org.freedesktop.Cozy.Manager') != '':
-					item = nautilus.MenuItem(name="NautilusPython::next", label="Next Version", tip="Go to next Version of current location", icon='go - next')
+					item = nautilus.MenuItem(name="NautilusPython::next", label="Next Version", tip="Go to next Version of current location", icon='go-next')
 					item.connect("activate", self.go_to_next_version, path, window)
 					items.append(item)
 		except Exception, e: # This must be something like a DBus object not existing exception
