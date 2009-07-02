1104f3d753543c050fc34f55f64c340a7d47f8fa
peter <peter@pider>
2009-07-03 01:48:44 +0200

    Again improved design

--- cozy-backup.py
+++ cozy-backup.py
@@ -4,38 +4,6 @@ import sys
 
 from cozy.configuration import Configuration
 from cozy.backupprovider import BackupProvider
-from cozy.backup import Backup
-
-class Data(object):
-
-    def __init__(self, config):
-        self.config = config
-        if self.config.data_path is None:
-            sys.exit('Error: Aborting doing backup because location of data is not configured.')
-
-    def back_up_to(self, backup):
-
-        if self.config.full_backup_path is None or self.config.backup_id is None:
-            exit('Error: Aborting doing backup because getting the backup configuration failed')
-        try:
-            filesystem = backup.mount_latest(config.full_backup_path, config.backup_id)
-
-            errors = filesystem.sync_with(config.data_path)
-
-            if len(errors) > 0:
-                sys.stderr.write(str(errors))
-                sys.exit(1)
-
-        except Backup.MountException, e:
-            sys.stderr.write(str(e) + "\n"
-                             'Error: Aborting backup because mounting failed')
-            sys.exit(2)
-        except Exception, e:
-            sys.stderr.write(str(e) + "\n"
-                             'Error: Aborting backup')
-            sys.exit(3)
-
-        backup.clone(config.full_backup_path, config.backup_id)
 
 
 if __name__ == '__main__':
@@ -58,3 +26,4 @@ if __name__ == '__main__':
         data = Data(config)
         data.back_up_to(backup)
 
+        backup.clone(config.full_backup_path, config.backup_id)
